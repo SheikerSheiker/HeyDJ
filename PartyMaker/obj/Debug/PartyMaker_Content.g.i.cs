4eddad3f4699fb1168264b9c6d09dde4db39ed67
Lera <forunimail@gmail.com>
2021-06-08 23:36:39 +0500

    Add 2nd slider and program icon

--- PartyMaker/obj/Debug/PartyMaker_Content.g.i.cs
+++ PartyMaker/obj/Debug/PartyMaker_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("beer3.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("beer_ico.ico")]
 
 
