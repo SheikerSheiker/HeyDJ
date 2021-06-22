a547751f555ab83a4a73536502d334d246c5cb8f
Max <maks.dudarev@mail.ru>
2021-06-22 22:05:12 +0500

    add price with kopeyki and add save & load data

--- PartyMaker/MainWindow.xaml.cs
+++ PartyMaker/MainWindow.xaml.cs
@@ -16,6 +16,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml.Serialization;
+using System.IO;
+using Microsoft.Win32;
 
 namespace PartyMaker
 {
@@ -68,7 +70,7 @@ namespace PartyMaker
         //        allAlco.Add(alco3);
         //    }
 
-            
+
 
 
 
@@ -81,40 +83,46 @@ namespace PartyMaker
         //Обработчики клавиш и вставки (ну тут не могу не принять, обработчик клавиш хорошо написал Макс, по сравнению с генератором :)  )
         private void AlcoPrice_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            if (!char.IsDigit(e.Text, 0))
+            if ((!char.IsDigit(e.Text, 0)) && (e.Text != ","))
                 e.Handled = true;
-            if ((e.Text=="0") && ((sender as TextBox).SelectionStart==0) && ((sender as TextBox).Text != "") || (((sender as TextBox).Text != "") && ((sender as TextBox).Text[0]=='0')))
+            if (((e.Text == ",") || (e.Text == "0")) && ((sender as TextBox).SelectionStart == 0) && ((sender as TextBox).Text != "") || (((sender as TextBox).Text != "") && ((sender as TextBox).Text[0] == '0')))
+                e.Handled = true;
+            if (!double.TryParse((sender as TextBox).Text + e.Text, out double a))
+                e.Handled = true;
+            if ((e.Text == ",") && ((sender as TextBox).Text.Length - (sender as TextBox).SelectionStart) > 2)
                 e.Handled = true;
             if ((sender as TextBox).Text == "0")
             {
                 (sender as TextBox).Text = e.Text;
                 (sender as TextBox).SelectionStart = (sender as TextBox).Text.Length;
             }
+            if (((sender as TextBox).Text.Contains(",")) && ((sender as TextBox).Text.Length - (sender as TextBox).Text.IndexOf(',') > 2))
+                e.Handled = true;
         }
         private void AlcoPrice_TextChanged(object sender, TextChangedEventArgs e)
         {
-                if ((!int.TryParse((sender as TextBox).Text, out int a)) || (int.Parse((sender as TextBox).Text)<0))
-                    (sender as TextBox).Text = "";
-                switch ((sender as TextBox).Name.ToString())
-            {
-                case "BeerPrice":
-                    {
-                        alcoResults[0].PriceBottle = (sender as TextBox).Text;
-                        break;
-                    }
-                case "CiderPrice":
-                    {
-                        alcoResults[1].PriceBottle = (sender as TextBox).Text;
-                        break;
-                    }
-                case "WinePrice":
-                    {
-                        alcoResults[2].PriceBottle = (sender as TextBox).Text;
-                        break;
-                    }
-                default:
-                    break;
-            }
+            //    if ((!int.TryParse((sender as TextBox).Text, out int a)) || (int.Parse((sender as TextBox).Text)<0))
+            //        (sender as TextBox).Text = "";
+            //    switch ((sender as TextBox).Name.ToString())
+            //{
+            //    case "BeerPrice":
+            //        {
+            //            alcoResults[0].PriceBottle = (sender as TextBox).Text;
+            //            break;
+            //        }
+            //    case "CiderPrice":
+            //        {
+            //            alcoResults[1].PriceBottle = (sender as TextBox).Text;
+            //            break;
+            //        }
+            //    case "WinePrice":
+            //        {
+            //            alcoResults[2].PriceBottle = (sender as TextBox).Text;
+            //            break;
+            //        }
+            //    default:
+            //        break;
+            //}
         }
 
        
@@ -134,7 +142,10 @@ namespace PartyMaker
                 return;
             }
             if (int.Parse((sender as TextBox).Text) > 500)
+            {
                 (sender as TextBox).Text = "500";
+                (sender as TextBox).SelectionStart = (sender as TextBox).Text.Length;
+            }
         }
         private void CharSpace_PreviewKeyDown (object sender, KeyEventArgs e)
         {
@@ -173,5 +184,71 @@ namespace PartyMaker
         {
             WinePriceWatermark.Text = $"Цена за {((ComboBoxItem)((sender as ComboBox).SelectedItem)).Content.ToString()}";
         }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog ofd = new SaveFileDialog();
+            ofd.FileName = "Document";
+            ofd.DefaultExt = ".text";
+            ofd.Filter = "Text documents (.txt)|*.txt";
+            ofd.ShowDialog();
+
+            StreamWriter f = new StreamWriter(ofd.FileName);
+            f.WriteLine(BeerSlider.Value);
+            f.WriteLine(BeerSize.Text);
+            f.WriteLine(BeerPrice.Text);
+            f.WriteLine(BeerCount.Text);
+            f.WriteLine(CiderSize.Text);
+            f.WriteLine(CiderPrice.Text);
+            f.WriteLine(CiderCount.Text);
+            f.WriteLine(WineSize.Text);
+            f.WriteLine(WinePrice.Text);
+            f.WriteLine(WineCount.Text);
+            f.WriteLine(AlcoSlider.Value);
+            f.WriteLine(Name1.Text);
+            f.WriteLine(Price1.Text);
+            f.WriteLine(Count1.Text);
+            f.WriteLine(Name2.Text);
+            f.WriteLine(Price2.Text);
+            f.WriteLine(Count2.Text);
+            f.WriteLine(Name3.Text);
+            f.WriteLine(Price3.Text);
+            f.WriteLine(Count3.Text);
+            f.Close();
+        }
+
+        private void Load_Click_2(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text documents (.txt)|*.txt";
+            ofd.ShowDialog();
+            if (ofd.FileName != "") // проверка на выбор файла
+            {
+                StreamReader f = new StreamReader(ofd.FileName);
+                BeerSlider.Value = double.Parse(f.ReadLine());
+                BeerSize.Text = f.ReadLine();
+                BeerPrice.Text = f.ReadLine();
+                BeerCount.Text = f.ReadLine();
+                CiderSize.Text = f.ReadLine();
+                CiderPrice.Text = f.ReadLine();
+                CiderCount.Text = f.ReadLine();
+                WineSize.Text = f.ReadLine();
+                WinePrice.Text = f.ReadLine();
+                WineCount.Text = f.ReadLine();
+                AlcoSlider.Value = double.Parse(f.ReadLine());
+                Name1.Text = f.ReadLine();
+                Price1.Text = f.ReadLine();
+                Count1.Text = f.ReadLine();
+                Name2.Text = f.ReadLine();
+                Price2.Text = f.ReadLine();
+                Count2.Text = f.ReadLine();
+                Name3.Text = f.ReadLine();
+                Price3.Text = f.ReadLine();
+                Count3.Text = f.ReadLine();
+                f.Close();
+            }
+            else MessageBox.Show("Файл не выбран");        
+        }
+
     }
 }
