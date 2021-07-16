0c72b63ed790a14eae8d908d88bbd38bd10613b4
Mirajiik <mirajiik@yandex.ru>
2021-07-17 04:14:48 +0500

    Remake NET Core, add git_ignore and animation top button's

---
+++ PartyMaker NET Core/ViewModels/SimpleWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PartyMaker_NET_Core.ViewModels.Base;
+
+namespace PartyMaker_NET_Core.ViewModels
+{
+    internal class SimpleWindowViewModel : ViewModel
+    {
+
+    }
+}
