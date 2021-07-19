9072a1c894f39382604e5fe2a401267f2ca2c505
Mirajiik <mirajiik@yandex.ru>
2021-07-20 02:32:04 +0500

    Add animations and functional buttons

---
+++ PartyMaker NET Core/Commands/Base/LambdaCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyMaker_NET_Core.Commands.Base
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+        public override bool CanExecute(object parametr) => _CanExecute?.Invoke(parametr) ?? true;
+        public override void Execute(object parameter) => _Execute(parameter);
+    }
+}
--- PartyMaker NET Core/ViewModels/SimpleWindowViewModel.cs
+++ PartyMaker NET Core/ViewModels/SimpleWindowViewModel.cs
@@ -1,14 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Win32;
+using PartyMaker_NET_Core.Commands.Base;
 using PartyMaker_NET_Core.ViewModels.Base;
 
 namespace PartyMaker_NET_Core.ViewModels
 {
     internal class SimpleWindowViewModel : ViewModel
     {
+        #region Команды
+        #region Сохранение профиля
+        public ICommand SaveProfileCommand { get; }
+        public bool CanSaveProfileCommandExecute(object p) => true;
+        public async void OnSaveProfileCommandExecuted(object p)
+        {
+            await Task.Run(() =>
+            {
+                SaveFileDialog ofd = new SaveFileDialog
+                {
+                    FileName = "Profile",
+                    DefaultExt = ".text",
+                    Filter = "Text documents (.txt)|*.txt"
+                };
+                ofd.ShowDialog();
 
+                StreamWriter f = new StreamWriter(ofd.FileName);
+                //Сюды добавить сохранение профиля
+                f.Close();
+            });
+        }
+        #endregion
+        #region Открытие профиля
+        public ICommand OpenProfileCommand { get; }
+        public bool CanOpenProfileCommandExecute(object p) => true;
+        public async void OnOpenProfileCommandExecuted(object p)
+        {
+            await Task.Run(() =>
+            {
+                OpenFileDialog ofd = new OpenFileDialog
+                {
+                    Filter = "Text documents (.txt)|*.txt"
+                };
+                ofd.ShowDialog();
+                if (ofd.FileName != "") // проверка на выбор файла
+                {
+                    StreamReader f = new StreamReader(ofd.FileName);
+                    try
+                    {
+                        //А сюды добавить чтение
+                        f.Close();
+                    }
+                    catch (Exception)
+                    {
+                        //Сюда добавить восстановление предыдущих значений, если входные данные не верны
+                        MessageBox.Show("Неверный формат входных данных!");
+                    }
+                }
+            });
+        }
+        #endregion
+        public ICommand OpenReferenceWindowCommand { get; }
+        public bool CanOpenReferenceWindowCommandExecute(object p) => true;
+        public async void OnOpenReferenceWindowCommandExecuted(object p)
+        {
+            await Task.Run(() =>
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    Reference refer = new Reference();
+                    refer.Show();
+                });
+            });
+        }
+        #endregion
+
+
+        public SimpleWindowViewModel()
+        {
+            #region Инициализация комманд
+            SaveProfileCommand = new LambdaCommand(OnSaveProfileCommandExecuted, CanSaveProfileCommandExecute);
+            OpenProfileCommand = new LambdaCommand(OnOpenProfileCommandExecuted, CanOpenProfileCommandExecute);
+            OpenReferenceWindowCommand = new LambdaCommand(OnOpenReferenceWindowCommandExecuted, CanOpenReferenceWindowCommandExecute);
+            #endregion
+        }
     }
 }
